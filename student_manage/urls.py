65fde6fdd4895a47b2fd2acabdf152ef7f0ef0cc
itmadityajoshi <adityajoshi316@gmail.com>
2024-09-07 16:04:09 +0545

    update logic build and html file created

--- student_manage/urls.py
+++ student_manage/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.list_student, name="home")
+    path('', views.list_student, name="home"),
+    path('update/<int:id>', views.update_student, name="update"),
 ]
--- student_manage/views.py
+++ student_manage/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Student
+from .forms import StudentInfoForm
 # Create your views here.
 
 
 def list_student(request):
     student = Student.objects.all() 
     return render(request, 'crud/list_student.html',{"student":student})
+
+
+def update_student(request,id):
+    if request.method == "POST":
+     student=Student.objects.get(pk=id)
+     fm=StudentInfoForm(request.POST, instance=student)
+     if fm.is_valid():
+        fm.save()
+        return redirect("/")
+    else: 
+        student=Student.objects.get(pk=id) 
+        fm = StudentInfoForm(instance=student)
+
+    return render(request, "crud/update_student.html",{"form":fm })
+
+
+def delete(request,id):
+   stude
